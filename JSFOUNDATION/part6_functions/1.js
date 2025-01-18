f3fdd99d5444d7e4178110559c1e449d0733774f
23yuvi23 <yogeshpaliwal9493@gmail.com>
2025-01-18 21:38:40 +0530

    18-jan-2025

---
+++ JSFOUNDATION/part6_functions/1.js
@@ -0,0 +1,11 @@
+/*
+write a function named "makeTea" that takes ine paramenter , "typeOfTea" and returns a string like "Making green tea " when called with "green tea".
+store the result in a variable named "tea order".
+*/
+
+function makeTea(typeOfTea){
+    return `Making ${typeOfTea} `
+    
+}
+
+console.log (makeTea("green tea"))
---
+++ JSFOUNDATION/part6_functions/3.js
@@ -0,0 +1,12 @@
+/*
+write an arrow function named "calculateTotal" that tales two parameters: "price" and "quantity".
+The function should return the total cost by multiplying the "price" and "quantitty"
+store the result in a vari naed "totalCost" 
+*/ 
+
+const calculateTotal =(price, quantity)=>  price * quantity
+    
+
+
+let totalCost = calculateTotal(499 , 100)
+console.log(totalCost)
